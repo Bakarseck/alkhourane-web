36a8d32d8c0d3baf74cd20fac8d5a80bbf94d7d3
bakseck <seck.bakar@ugb.edu.sn>
2025-02-12 15:47:20 +0000

    alkhourane

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ["hebbkx1anhila5yf.public.blob.vercel-storage.com"],
     },
+    eslint: {
+        ignoreDuringBuilds: true, // ✅ Désactive ESLint lors de la compilation
+    },
 };
 
 export default nextConfig;
